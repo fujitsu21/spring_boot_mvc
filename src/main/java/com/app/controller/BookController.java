65f6316292c0eb37589dc655ee0cafdedc88ef6d
fujitsu21 <railsnewapp@outlook.com>
2021-02-22 14:05:55 +0900

    bootstrap and data restore

--- src/main/java/com/app/controller/BookController.java
+++ src/main/java/com/app/controller/BookController.java
@@ -1,28 +1,85 @@
 package com.app.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.app.model.Book;
+import com.app.repository.BookRepository;
 
 @Controller
 public class BookController {
 	
-	@GetMapping("/")
-	public String index(@ModelAttribute Book book) {
-		return "index";
+	// repositoryインターフェースを自動インスタンス化
+	@Autowired
+	private BookRepository bookstore;
+	
+	/**
+	 * [list] へアクセスがあった場合
+	 */
+	@RequestMapping("/list")
+	public ModelAndView list(ModelAndView mav) {
+		// bookstoreテーブルから全件取得
+		Iterable<Book> book_list = bookstore.findAll();
+		
+		// Viewに渡す変数をModelに格納
+		mav.addObject("book_list", book_list);
+		
+		// 画面に出力するViewを指定
+		mav.setViewName("list");
+		
+		// return Model and View
+		return mav;
 	}
 	
-	@PostMapping("/")
-	public String confirm(@Validated @ModelAttribute Book book, BindingResult result) {
+	
+	/**
+	 * [index]へアクセスがあった場合
+	 */
+	@RequestMapping("/insert")
+	public ModelAndView index(@ModelAttribute Book book, ModelAndView mav) {
+		// store various for View to Model
+		mav.addObject("book", book);
+		
+		// 画面に出力するViewを指定
+		mav.setViewName("insert");
+		
+		// return Model and View
+		return mav;
+	}
+	
+	/**
+	 * [insert]へPOST送信された場合
+	 */
+	@RequestMapping(value = "/insert", method = RequestMethod.POST)
+	// POSTデータをBookインスタンスとして受け取る
+	public ModelAndView insertPost(@ModelAttribute @Validated Book book, BindingResult result, ModelAndView mav) {
+		
+		// if input error exist
 		if(result.hasErrors()) {
-			// if error exist => index.html
-			return "index";
+			// error message
+			mav.addObject("message", "入力内容に誤りがあります");
+			
+			//　画面に出力するViewを指定
+			mav.setViewName("insert");
+			
+			// return Model and View
+			return mav;
 		}
-		return "confirm";
+		
+		
+		// store to db input data;
+		bookstore.saveAndFlush(book);
+		
+		// redirect
+		mav = new ModelAndView("redirect:/list");
+		
+		// return Model and View
+		return mav;
 	}
 }
--- src/main/java/com/app/model/Book.java
+++ src/main/java/com/app/model/Book.java
@@ -1,33 +1,65 @@
 package com.app.model;
 
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Getter
-@Setter
-public class Book {
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="bookstore")
+
+public class Book implements Serializable {
 	
-	 @NotBlank
-	 @Size( max = 20)
-	 private String employee_number;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * ID
+	 */
+	@Id
+	@Column
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank
+	@Column
+	@Size( max = 20)
+	
+	private String employee_number;
 
     // 必須入力、Email形式であること、文字列が30文字まで。
     @NotBlank
     @Email
     @Size( max = 30)
+    @Column
     private String employee_mail;
 
     // 必須入力、文字列が20文字まで。
     @NotBlank
     @Size( max = 20)
+    @Column
     private String book_name;
     
     // 必須入力、文字列が50文字まで。
     @NotBlank
     @Size( max = 20)
+    @Column
     private String jan_code;
 }
